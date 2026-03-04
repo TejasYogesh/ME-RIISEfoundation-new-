8ca377b3a0f22568882bc876f381278a23e5abed
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2026-03-04 18:10:34 +0530

    testing once again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -211,7 +211,7 @@ export function NavigationMenuDemo() {
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
               <Link to="/" className={navigationMenuTriggerStyle()}>
-                Home
+                Home in ME-RIISE
               </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
